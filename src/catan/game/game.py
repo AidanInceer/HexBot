d61f93b96e867faa739e18904e1903f2b30b8360
AidanInceer <aidaninceer0@gmail.com>
2025-03-25 14:50:28 +0000

    refactor auto setup and robber functionality

--- src/catan/game/game.py
+++ src/catan/game/game.py
@@ -7,6 +7,7 @@ from src.catan.buildings.buildings import City, Settlement
 from src.catan.deck.deck import CardDeck
 from src.catan.player.player import Player
 from src.catan.resources.resources import Brick, Ore, Sheep, Wheat, Wood
+from src.catan.game.robber.robber_manager import RobberManager
 from src.config.config import CentralConfig
 from src.interface.input_handler import InputHandler
 
@@ -27,6 +28,9 @@ class Game:
     config: CentralConfig
     board: Board = field(default_factory=Board)
     game_ended: bool = False
+
+    def __post_init__(self):
+        self.robber_manager = RobberManager(self.board, self.players)
 
     def run(self) -> None:
         """
@@ -184,96 +188,4 @@ class Game:
         Returns:
             None
         """
-        # move the robber to a different tile
-        new_robber_tile = self.move_robber_tile()
-
-        # choose a player to steal a random resource from
-        # if the player has no resources, they are skipped
-        able_to_steal_from = self.determine_who_to_steal_from(
-            current_player, new_robber_tile
-        )
-
-        if able_to_steal_from:
-            robbed_player = self.select_player_to_steal_from(able_to_steal_from)
-            if robbed_player:
-                self.steal_random_resource(robbed_player, current_player)
-
-        # if any player has more than 7 cards, they must discard half
-        self.robber_discard_resources()
-
-    def move_robber_tile(self) -> Tile:
-        robber_tile = self.board.get_robber_tile()
-        robber_moved = False
-        while not robber_moved:
-            move_robber = int(input("Choosen a Tile [0-18] to move the Robber to: "))
-            if move_robber < 0 or move_robber > 18:
-                print("Invalid tile.")
-            elif move_robber == robber_tile.id:
-                print("Robber must be moved to a different tile.")
-            else:
-                new_robber_tile = self.board.set_robber_tile(move_robber)
-                new_robber_tile.robber = True
-                robber_tile.robber = False
-                robber_moved = True
-
-        return new_robber_tile
-
-    def select_player_to_steal_from(self, able_to_steal_from: set) -> Player:
-        select_player = input(f"Choosen a player to steal from {able_to_steal_from}: ")
-
-        robbed_player = [
-            player
-            for player in self.players
-            if player.color in select_player and player.total_resources() > 0
-        ]
-        if len(robbed_player) == 0:
-            print("No players to steal from.")
-
-        return robbed_player
-
-    def steal_random_resource(
-        self, robbed_player: Player, current_player: Player
-    ) -> None:
-        robbed_player = robbed_player[0]
-
-        # Get the available resources of the robbed player
-        available_resources = [
-            resource_name.lower()
-            for resource_name, resource in robbed_player.resources.__dict__.items()
-            if isinstance(resource, TYPES) and resource.count > 0
-        ]
-
-        # Choose a random resource to steal
-        random_resource = random.choice(list(available_resources))
-
-        # Update the resource counts for the current player and the robbed player
-        robbed_player.resources[random_resource].count -= 1
-        current_player.resources[random_resource].count += 1
-
-        print(
-            f"{current_player.color} stole {random_resource} from {robbed_player.color}."
-        )
-
-    def determine_who_to_steal_from(
-        self, current_player: Player, new_robber_tile: Tile
-    ) -> Player:
-        adjacent_nodes = new_robber_tile.get_near_nodes()
-
-        able_to_steal_from = set()
-        for node in adjacent_nodes:
-            if isinstance(self.board.nodes[node].building, (Settlement, City)):
-                able_to_steal_from.add(self.board.nodes[node].building.color)
-
-        if current_player.color in able_to_steal_from:
-            able_to_steal_from.remove(current_player.color)
-
-        if len(able_to_steal_from) == 0:
-            print("No players to steal from.")
-
-        return able_to_steal_from
-
-    def robber_discard_resources(self) -> None:
-        for player in self.players:
-            total = player.total_resources()
-            if total > 7:
-                player.discard_resources(total)
+        self.robber_manager.activate_robber(current_player)
